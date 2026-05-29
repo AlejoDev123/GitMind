5411131e41b0df951f1bff0d3767b0f92f8cd4aa
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-05-29 10:56:20 -0500

    feat: rename project title to Git Mind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-blue-600 text-[5rem] text-center">
-        GitMind
+        Git Mind
       </h1>
       <p className="text-[18px] text-gray-700">
         React + Tailwind CSS project
